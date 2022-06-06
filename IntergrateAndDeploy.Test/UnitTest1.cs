109e449249bcf46e4c5a00a4063e02603027c936
Sengleak <mrsengleak@gmail.com>
2022-06-07 00:05:44 +0700

    Add AllTest02

--- IntergrateAndDeploy.Test/UnitTest1.cs
+++ IntergrateAndDeploy.Test/UnitTest1.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IntergrateDatabase.api.Controllers;
+using IntergrateDatabase.api.CustomerAndContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace IntergrateAndDeploy.Test
@@ -8,7 +14,41 @@ namespace IntergrateAndDeploy.Test
         [Fact]
         public void Test1()
         {
+            Assert.True(1==1);
+        }
+
+        [Fact]
+        public async Task CustomerIntergrationTest()
+        {
+            // Create DB Context
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+ 
+            var optionsBuilder = new DbContextOptionsBuilder<CustomerContext>();
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            var context = new CustomerContext(optionsBuilder.Options);
+
+            //Make sure to Delete all Existing Customers in Databse
+                //context.customers.RemoveRange(await context.customers.ToArrayAsync());
+                //await context.SaveChangesAsync();
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            // Create Controller
+            var controller = new CustomerController(context);
+
+            //Add Customer
+            await controller.CreateCustomer(new Customer()
+            {
+                CustomerName = "Sengleak"
+            });
+
+            //Check if GetAll() return the added customer
+            var result = (await controller.GetAll()).ToArray();
+            Assert.Single(result);
+            Assert.Equal("Sengleak", result[0].CustomerName);
 
         }
     }
+    
 }
